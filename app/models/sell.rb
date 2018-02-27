f7f34a322194bb131674000890a08ae6970dfab1
Randson Oliveira <orandson@gmail.com>
2018-02-27 01:02:18 -0400

    customize a litter bit of behavior of clients and sellers

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,11 +1,16 @@
 class Sell < ApplicationRecord
   include Fae::BaseModelConcern
 
-  def fae_display_field
-    
-  end
-
-
   belongs_to :discount
   belongs_to :client
+
+  enum status: { finished: 0, canceled: 1 }
+
+  def fae_display_field
+    id
+  end
+
+  def self.for_fae_index
+    order(:id)
+  end
 end
