1334cab71147dd7ac009ddb2b149aa5d3d3024a2
Randson Oliveira <orandson@gmail.com>
2018-03-18 17:05:07 -0400

    calculate the total of products and services

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -14,11 +14,29 @@ class Sell < ApplicationRecord
 
   validates :client, presence: true
 
+  before_save :set_total
+
   def fae_display_field
     id
   end
 
   def self.for_fae_index
     order(:id)
+  end
+
+  private
+
+  def set_total
+    total = 0
+
+    self.products.each { |p| total += p.price }
+    self.services.each { |s| total += s.price }
+
+    if self.discount.present?
+      total = total - self.discount.value
+    end
+
+    total = (total > 0) ? total : 0
+    self.total = total
   end
 end
